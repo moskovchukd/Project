75635dbca68e9dadbb1ba91a47cfe13e9db80657
Danya <danylomoskovchuk@gmail.com>
2024-01-09 09:42:02 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,76 @@
+"use strict";
+
+//ТИПИ ДАНИХ
+
+
+/*let number = 4.6;
+console.log(-4/0);
+console.log('string'*9);
+const person = `Alex`;
+console.log(person);
+const bool = true;
+//console.log(something);
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+
+
+};
+
+console.log(obj['name']);
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[0]);*/
+
+//СПІЛКУВАННЯ З КОРИСТУВАЧЕМ
+
+//alert('Hello!');
+//const result = confirm('Are you here?');
+//console.log(result);
+//const answer = prompt('Are you 18 y. o.?', '18');
+//console.log(answer + 5);
+
+/*
+const answers = [];
+answers[0] = prompt("What is your name?", "");
+answers[1] = prompt("What is your surname?", "");
+answers[2] = prompt("What is your age?", "");
+
+console.log(typeof(answers));
+console.log(typeof(null))
+*/
+
+//ІНТЕРПОЛЯЦІЯ
+
+/*const category = 'toys';
+console.log(`ttps://someurl.com/${category}/5`);
+const user = 'Danya';
+alert(`HEllo, ${user}`);
+*/
+
+//ОПЕРАТОРИ В JS
+
+console.log('arr' + '- obj');
+console.log(4 + '-obj');
+console.log(4 + +'-obj');
+console.log(4 + +'5');
+
+let incr = 10,
+    decr = 10;
+//++incr;
+//--decr;
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === '8');
+
+const isChecked = false,
+      isClose = true;
+console.log(isChecked || isClose);
+console.log(isChecked && isClose);
+
